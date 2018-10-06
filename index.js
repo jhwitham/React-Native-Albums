6c123733d25cb44ce7bd2cf568294cf89d6ae437
John Herman Witham <john.h.witham@gmail.com>
2018-10-06 15:39:22 +0300

    Add App Component /w text & render component

--- index.js
+++ index.js
@@ -3,5 +3,11 @@ import React from 'react';
 import ReactNative from 'react-native';
 
 // Create a Component
+const App = () => {
+  return (
+    <Text>Some Text</Text>
+  );
+};
 
 // Render it to the device
+ReactNative.AppRegistry.registerComponent('albums', () => App);
