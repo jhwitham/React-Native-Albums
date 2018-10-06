687bc195d6ecc62590f7f06dc9a16c91c3cfd489
John Herman Witham <john.h.witham@gmail.com>
2018-10-06 15:44:02 +0300

    Import Text property and AppRegistry

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Import a library to help create a Component
 import React from 'react';
-import ReactNative from 'react-native';
+import { Text, AppRegistry } from 'react-native';
 
 // Create a Component
 const App = () => {
@@ -10,4 +10,4 @@ const App = () => {
 };
 
 // Render it to the device
-ReactNative.AppRegistry.registerComponent('albums', () => App);
+AppRegistry.registerComponent('albums', () => App);
